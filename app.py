500a72ca7f5cf19bc8d71d742d8ecacc52b92d06
Sripaad srinivasan <sripaad751@gmail.com>
2020-07-05 03:12:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, request, render_template
-import mainFile
 
+from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+set(stopwords.words('english'))
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,10 +16,16 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    
-    text1 = request.form['text1'].lower()
-    compound = mainFile.runPrediction(text1)
-    return render_template('form.html', final=compound, text1=text1)
+    stop_words = stopwords.words('english')
+    test_data = request.form['test_data'].lower()
+
+    processed_data = ' '.join([word for word in test_data.split() if word not in stop_words])
+
+    sia = SentimentIntensityAnalyzer()
+    dd = sia.polarity_scores(text=processed_data)
+    compound = round((1 + dd['compound'])/2, 2)
+
+    return render_template('form.html', final=compound, test_data=test_data)
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5002, threaded=True)
